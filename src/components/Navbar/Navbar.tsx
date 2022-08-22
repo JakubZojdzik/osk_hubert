667ee5f80d9eeff7f28f755db8dca843a4d66238
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-22 23:02:39 +0200

    Add `Navbar.tsx`

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,25 @@
-// import { Switch } from '@mui/material';
-
 
 export const Navbar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg border-bottom mb-5">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/#">osk HUBERT</a>
+                <a className="navbar-brand ms-3" href="/#">osk HUBERT</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="mainNavbar">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-3">
                             <a className="nav-link" href="/#">Home</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item mx-3">
                             <a className="nav-link" href="/#">Oferta</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Kategorie
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="/#">AM</a></li>
-                                <li><a className="dropdown-item" href="/#">A1</a></li>
-                                <li><a className="dropdown-item" href="/#">A2</a></li>
-                                <li><a className="dropdown-item" href="/#">A</a></li>
-                                <li><a className="dropdown-item" href="/#">B</a></li>
-                            </ul>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="/#">Kategorie</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item mx-3">
                             <a className="nav-link" href="/#">Kontakt</a>
                         </li>
                     </ul>
