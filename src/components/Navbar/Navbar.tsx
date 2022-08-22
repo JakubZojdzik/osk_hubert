e7eb69894fb260ee7b21695db26ba124b34b7464
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-22 21:51:01 +0200

    Fix some attributes in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,13 +7,13 @@ export const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/#">osk HUBERT</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="mainNavbar">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
+                            <a className="nav-link" href="/#">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/#">Oferta</a>
@@ -31,7 +31,7 @@ export const Navbar = () => {
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="/#" aria-disabled="true">Kontakt</a>
+                            <a className="nav-link" href="/#">Kontakt</a>
                         </li>
                     </ul>
                 </div>
