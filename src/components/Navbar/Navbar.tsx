a3d03b1e19bccc77ed1110470c07815f74260cc1
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-24 14:04:07 +0200

    Close `Navbar` into Container

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,48 @@
+import { Container } from "react-bootstrap";
 
 export const Navbar = () => {
-
     return (
         <nav className="navbar navbar-expand-lg border-bottom mb-5">
-            <div className="container-fluid">
-                <a className="navbar-brand ms-3 text-primary" href="/#">osk HUBERT</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <Container>
+                <a className="navbar-brand ms-3 text-primary" href="/#">
+                    osk HUBERT
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#mainNavbar"
+                    aria-controls="mainNavbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="mainNavbar">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item mx-3">
-                            <a className="nav-link" href="/#">Home</a>
+                            <a className="nav-link" href="/#">
+                            Home
+                            </a>
                         </li>
                         <li className="nav-item mx-3">
-                            <a className="nav-link" href="/#">Oferta</a>
+                            <a className="nav-link" href="/#">
+                            Oferta
+                            </a>
                         </li>
                         <li className="nav-item mx-3">
-                            <a className="nav-link" href="/#kategorie">Kategorie</a>
+                            <a className="nav-link" href="/#kategorie">
+                            Kategorie
+                            </a>
                         </li>
                         <li className="nav-item mx-3">
-                            <a className="nav-link" href="/#">Kontakt</a>
+                            <a className="nav-link" href="/#">
+                            Kontakt
+                            </a>
                         </li>
                     </ul>
                 </div>
-            </div>
+            </Container>
         </nav>
     );
 };
