58f8f3eb609899e5330ec5d85b1271f3568500b8
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-24 12:22:54 +0200

    Create `Banner` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Categories, Navbar } from "./components";
+import { Banner, Categories, Navbar } from "./components";
 
 function App() {
     return (
         <div className="App">
             <Navbar></Navbar>
+            <Banner></Banner>
             <div className="container">
                 <Categories></Categories>
             </div>
---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,12 @@
+
+export const Banner = () => {
+    return (
+        <article>
+            <div id="onas" className="naglowek">
+                <div>
+                    <h1 className="tytul">Ośrodek Szkolenia Kierowców "HUBERT"</h1>
+                </div>
+            </div> 
+        </article>
+    )
+}
