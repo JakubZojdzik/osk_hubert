e470a3c7fe4a858afe765e1e11f57f5151fab847
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-24 20:53:43 +0200

    Compleate `Contact`

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,11 +5,11 @@ export const Contact = () => {
     return (
         <Container id="kontakt" className="mt-5">
             <Row>
-                <Col lg={true}>
-                    <h3>
+                <Col lg={3} xs={12}>
+                    <h3 className="text-primary">
                         Skontaktuj się
                     </h3>
-                    <hr className="bold-hr" />
+                    <hr className="bold-hr text-primary" />
                     <div className="info">
                         <p className="address">
                             59-600 Lwówek Śląski<br />ul. Słoneczna 18
@@ -20,6 +20,11 @@ export const Contact = () => {
                         <p className="mail">
                             biuro@oskhubert.pl
                         </p>
+                    </div>
+                </Col>
+                <Col xs={12} lg={9} className="col-8">
+                    <div className="embed-responsive">
+                        <iframe className="yt" src="https://www.youtube.com/embed/3Y1_9CNEiks" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                     </div>
                 </Col>
             </Row>
