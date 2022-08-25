587b7541a3e1549f3fd4dbc62410bbd44eaeca66
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-25 22:52:19 +0200

    Now its everythink xd

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner = () => {
     return (
         <div className="container-fluid img-bg d-flex align-items-center text-center mb-5">
             <Container>
-                <h1 className="logo">Ośrodek Szkolenia Kierowców "HUBERT"</h1>
+                <h1 className="logo">Ośrodek Szkolenia Kierowców HUBERT</h1>
                 <p className="desc align-middle">
                     w Lwówku Śląskim to firma z wieloletnim doświadczaniem.<br />
                     Podczas naszej pracy zdobyliśmy renomę i zadowolenie kilku pokoleń kursantów,<br />
