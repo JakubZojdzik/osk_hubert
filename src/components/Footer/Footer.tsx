4824454c87bfc793f13afdca05f51249ac34fb98
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-25 15:55:23 +0200

    Fix `Footer` margin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <>
-            <svg style={{"marginTop": "-12rem"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <svg style={{"marginTop": "-5rem"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#AD2628" d="M0,288L120,288C240,288,480,288,720,282.7C960,277,1200,267,1320,261.3L1440,256L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
             </svg>
             <footer className="text-center bg-primary">
