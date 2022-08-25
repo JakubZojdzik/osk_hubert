bf80a527a6c7297263fd0651947722517ca92324
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-25 15:19:52 +0200

    Add `Blobl`

--- src/components/Blob/Blob.tsx
+++ src/components/Blob/Blob.tsx
@@ -3,8 +3,12 @@ import './xd';
 
 export const Blob = () => {
     return (
-        <div className="outer">
-            <div className="blob"> </div>
-        </div>
+        <>
+            <div id="blob-l-1" className="outer d-none d-lg-block blob-l"> </div>
+            <div id="blob-l-2" className="outer d-none d-lg-block blob-l"> </div>
+
+            <div id="blob-r-1" className="outer d-none d-lg-block blob-r"> </div>
+            <div id="blob-r-2" className="outer d-none d-lg-block blob-r"> </div>
+        </>
     );
 };
