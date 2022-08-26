e32a1605940ff8d0855ba6cadb98c5f39eed4ca9
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-26 15:30:03 +0200

    Style links in `Contact`

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -14,17 +14,17 @@ export const Contact = () => {
                     <address>
                         <div id="address" className="d-flex align-items-center mb-2">
                             <i className="bi bi-pin-map-fill p-2"></i>
-                            <p className="align-middle m-0"><a href="https://www.google.com/maps/place/HUBERT+O%C5%9Brodek+Szkolenia+Kierowc%C3%B3w/@51.1108687,15.6165572,15.38z/data=!4m8!1m2!3m1!2sHUBERT+O%C5%9Brodek+Szkolenia+Kierowc%C3%B3w!3m4!1s0x470f2f90084b6509:0xd7cb44a5ad7f647b!8m2!3d51.1135402!4d15.608705"> 59-600 Lwówek Śląski<br />ul. Słoneczna 18</a></p>
+                            <p className="align-middle m-0"><a rel="noreferrer" target="_blank" href="https://www.google.com/maps/place/HUBERT+O%C5%9Brodek+Szkolenia+Kierowc%C3%B3w/@51.1108687,15.6165572,15.38z/data=!4m8!1m2!3m1!2sHUBERT+O%C5%9Brodek+Szkolenia+Kierowc%C3%B3w!3m4!1s0x470f2f90084b6509:0xd7cb44a5ad7f647b!8m2!3d51.1135402!4d15.608705"> 59-600 Lwówek Śląski<br />ul. Słoneczna 18</a></p>
                         </div>
 
                         <div id="phone" className="d-flex align-items-center mb-2">
                             <i className="bi bi-telephone p-2"></i>
-                            <p className="align-middle m-0">504-177-700</p>
+                            <p className="align-middle m-0"><a rel="noreferrer" target="_blank" href="tel:504177700">504-177-700</a></p>
                         </div>
 
                         <div id="mail" className="d-flex align-items-center mb-2">
                             <i className="bi bi-envelope p-2"></i>
-                            <p className="align-middle m-0">biuro@oskhubert.pl</p>
+                            <p className="align-middle m-0"><a rel="noreferrer" target="_blank" href="mailto:biuro@oskhubert.pl">biuro@oskhubert.pl</a></p>
                         </div>
 
                         <div id="mail" className="d-flex align-items-center mb-2">
