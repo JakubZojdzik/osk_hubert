23052c9e463c6032e8919f194f5d959d85fecd4e
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-08-26 15:20:00 +0200

    Add date of organization meeting in `Break`

--- src/components/Break/Break.tsx
+++ src/components/Break/Break.tsx
@@ -5,9 +5,10 @@ import { Container } from "react-bootstrap"
 export const Break = () => {
     return (
         <div className="red_bg mt-5">
-            <Container>
+            <Container className="px-5">
                 <h1>Najbliższy termin spotkania organizacyjnego:</h1>
-                <p>Niestety, żadne spotkanie organizacyjne nie zostało zaplanowane.<br />Zadzwoń do mnie: 504-177-700</p>
+                <h1><b>16 września 2022r.</b></h1><br />
+                <p>Zadzwoń do mnie: 504-177-700</p>
             </Container>
         </div>
     );
